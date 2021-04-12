d4758584c31374b953ca88fb16a435854f1a2e9a
Michel Motta da Silva <michelmottadasilva@gmail.com>
2021-04-12 03:26:16 -0400

    Adicionada novas configurações e inclusão de arquivos a serem ignorados pelo gitignore

--- api/sonar-project.js
+++ api/sonar-project.js
@@ -3,7 +3,11 @@ sonarqubeScanner({
     serverUrl: 'http://sonarqube:9000',
     options : {
     'sonar.sources': '.',
-    'sonar.inclusions' : '/**', // Entry point of your code
+    'sonar.tests': '.',
+    'sonar.inclusions' : '**', // Entry point of your code
+    'sonar.test.inclusions': 'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+    'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+    'sonar.testExecutionReportPaths': 'coverage/test-reporter.xml',
     'sonar.login': 'admin',
     'sonar.password': 'admin'
 }}, () => {console.log('Escaneamento finalizado');});
