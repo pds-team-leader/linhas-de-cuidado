13c4f46bbfb18d1b8e33a5268a90cc0c2205b1df
Michel Motta da Silva <michelmottadasilva@gmail.com>
2021-04-12 03:29:54 -0400

    Retirada de configuraçoes para serem usadas quando estiverem definida a forma como serao feito os testes

--- app/sonar-project.js
+++ app/sonar-project.js
@@ -6,8 +6,6 @@ sonarqubeScanner({
     'sonar.tests': '.',
     'sonar.inclusions' : '**', // Entry point of your code
     'sonar.test.inclusions': 'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
-    'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
-    'sonar.testExecutionReportPaths': 'coverage/test-reporter.xml',
     'sonar.login': 'admin',
     'sonar.password': 'admin'
 }}, () => {console.log('Escaneamento finalizado');});
