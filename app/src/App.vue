41af07d6855954bb2d3638292849702070407510
Lucas <lucasoliveira96@outlook.com>
2021-05-25 17:15:13 -0400

    feat: instalando vuefity

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,29 @@
 <template>
-  <div id="app">
+<v-app>
+  <v-navigation-drawer app>
+    <!-- -->
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <!-- -->
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
     Mensagem do Servidor: {{ mensagem }}
-  </div>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
 </template>
 
 <script>
