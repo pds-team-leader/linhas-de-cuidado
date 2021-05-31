630c49a2d47d3b4ddc52067f1fe22f865aceadc7
Lucas <lucasoliveira96@outlook.com>
2021-05-31 17:03:59 -0400

    feat: cria rotas para os guias e adiciona breadcrumbs

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -6,6 +6,38 @@ const routes = [
     name: 'Home',
     component: () => import('../pages/Home.vue'),
   },
+  {
+    path: '/guia-diabetes',
+    name: 'GuiaDiabetes',
+    component: () => import('../pages/GuiaDiabetes.vue'),
+    meta: {
+      breadcrumbs: [
+        {
+          text: 'Home',
+          route: '/',
+        },
+        {
+          text: 'Guia Diabetes Mellitus',
+        },
+      ],
+    },
+  },
+  {
+    path: '/guia-hipertensao',
+    name: 'GuiaHipertensao',
+    component: () => import('../pages/GuiaHipertensao.vue'),
+    meta: {
+      breadcrumbs: [
+        {
+          text: 'Home',
+          route: '/',
+        },
+        {
+          text: 'Guia Hipertensão Arterial',
+        },
+      ],
+    },
+  },
 ];
 
 export default new VueRouter({
