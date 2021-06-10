c1462c2e522dc4797aac7583858ff38a809081c1
Lucas Oliveira <lucasoliveira96@outlook.com>
2021-06-10 18:46:01 -0400

    fix: config database

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  host: process.env === 'development' ? 'db' : process.env.PG_HOST,
+  host: process.env !== 'development' ? 'postgres' : process.env.PG_HOST,
   username: 'postgres',
   password: 'postgres',
   database: 'postgres',
