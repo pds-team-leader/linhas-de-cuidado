d80ac775b767f45bdf3fb73b3f124c2864ba6b6f
pds-team-leader <81944783+pds-team-leader@users.noreply.github.com>
2021-06-22 21:11:32 -0400

    Delete server 2.js

--- api/src/server 2.js
+++
@@ -1,9 +0,0 @@
-import app from './app';
-
-const PORT = process.env.PORT || 3000;
-
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`Server up at http://localhost:${PORT}`);
-  });
-}
